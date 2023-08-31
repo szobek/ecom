62c73922af1afff213a9c0a0f51839fa1e26cc8b
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-08-31 21:58:19 +0200

    Update web.php with start page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('list');
 });
 
 Route::get('/create', function () {
